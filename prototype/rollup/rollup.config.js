f8e580c8739c0f900eaf7c1dcb94300a62f1b26b
josephxia <15906475@qq.com>
2020-11-18 11:47:59 +0800

    update

--- prototype/rollup/rollup.config.js
+++ prototype/rollup/rollup.config.js
@@ -1,11 +1,32 @@
-export default {
-    input: 'src/entry.js',
-    output: {
-      file: 'dist/index.js',
-      name: 'Element',
-      format:'umd'
-    },
-    plugins: [
-    //   typescript(/*{ plugin options }*/)
-    ]
-  }
+const es = {
+  input: "src/entry.js",
+  output: {
+    file: "dist/index.js",
+    name: "Element",
+    format: "umd",
+  },
+};
+
+import babel from "@rollup/plugin-babel";
+import { terser } from 'rollup-plugin-terser'
+const minEs = {
+  input: "src/entry.js",
+  output: {
+    file: "dist/index.min.js",
+    name: "Element",
+    format: "umd",
+  },
+  plugins: [terser()],
+};
+
+const cjs = {
+  input: "src/entry.js",
+  output: {
+    file: "dist/index.cjs.js",
+    name: "Element",
+    format: "cjs",
+  },
+  plugins: [babel()],
+};
+
+export default [es, minEs, cjs];
